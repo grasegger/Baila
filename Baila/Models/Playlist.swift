d067cb6ab9705ac891ec1c41005ac32dc45770fc
Karl <51945878+grasegger@users.noreply.github.com>
2026-05-01 19:25:44 +0200

    remember current position in playlist

--- Baila/Models/Playlist.swift
+++ Baila/Models/Playlist.swift
@@ -11,10 +11,14 @@ import SwiftData
 class Playlist {
     @Attribute(.unique) var singletonID: String
     var tracks: [Track]
+    var currentTrack : Track?
+    var currentTrackPositionMS: Int
 
-    init(singletonID: String = Playlist.defaultSingletonID, tracks: [Track] = []) {
+    init(singletonID: String = Playlist.defaultSingletonID, tracks: [Track] = [], currentTrack: Track? = nil, currentTrackPositionMS: Int = 0) {
         self.singletonID = singletonID
         self.tracks = tracks
+        self.currentTrack = currentTrack
+        self.currentTrackPositionMS = currentTrackPositionMS
     }
 
     static let defaultSingletonID = "main"
